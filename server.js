5e5681c34f6752310b864bacdad39292f61e8256
Yujun Huang <yujun-huang@uiowa.edu>
2017-05-17 05:37:46 +0000

    production url

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ io.on('connection', function(socket){
 
 });
 
-server.listen(8080,function() {
+server.listen(process.env.PORT || 8080, process.env.IP || "0.0.0.0",function() {
    console.log("we are connected");
 });
 
